5b123eff65fe1949151d6b24a59e7a37fac99155
DoctorMod <davidgolding05@gmail.com>
2021-01-27 18:36:53 +0800

    Added Contact Page to my website

--- main.js
+++ main.js
@@ -91,3 +91,24 @@ let timer = setInterval(function() {
 
     document.getElementById("timer").innerHTML = zeros(years, 12);
 }, 10);
+
+const seenEl = document.querySelector('#lastseen');
+
+function updateSeen() {
+    fetch('seen.json?nocache=' + Math.random().toString().substring(2)).then(r => r.text()).then(r => {
+        let json = JSON.parse(r);
+        if (json.project) {
+            if (json.project_href)
+                // Please no XSS
+                seenEl.innerHTML = `I just finished working on <a href="${json.project_href}" class="d" target="_blank"
+                style="color:#949494;">${json.project}<span class="underline" style="background-color:#949494;"></span></a>.`;
+            else
+                seenEl.innerText = 'I just finished working on ' + json.project + '.';
+        } else {
+            seenEl.innerText = "I'm not working on anything right now."
+        }
+    }).catch(console.log);
+}
+
+setInterval(updateSeen, 60 * 1000);
+updateSeen();
